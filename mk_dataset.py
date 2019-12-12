4d591a54dcbf79529d133736ca10d2e79475aaae
N Syrotiuk <sefnyn@gmail.com>
2019-12-12 17:41:48 +0000

    added csv module and command-line parameter

--- mk_dataset.py
+++ mk_dataset.py
@@ -1,8 +1,23 @@
+import sys,csv
 from faker import Faker
 from faker.providers import date_time,job,lorem
 
 fake = Faker('en_GB')
 
-for _ in range(10):
-    dob = str(fake.date_of_birth(minimum_age=25, maximum_age=70))
-    print(fake.name() + '\t' +  fake.job() + '\t' + dob + '\t' + fake.text(max_nb_chars=40))
+if len(sys.argv) > 1:
+    default = int(sys.argv[1])
+else:
+    default = 10
+
+print('Making fake dataset with ' + str(default) + ' records')
+with open('dataset.csv', 'w', newline='') as csvfile:
+    mywriter = csv.writer(csvfile)
+    for _ in range(default):
+        record = []
+        dob = str(fake.date_of_birth(minimum_age=25, maximum_age=70))
+        record.append(fake.name())
+        record.append(fake.job())
+        record.append(dob)
+        record.append(fake.text(max_nb_chars=40))
+        mywriter.writerow(record)
+
